a2213d4a0b28d3fa64322a0db3f350d1c11712e1
Federico <federico.peyrani.fp@gmail.com>
2022-07-09 15:43:09 +0200

    feat(detekt): add Detekt config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.DetektPlugin
+
 buildscript {
     repositories {
         google()
@@ -12,6 +14,16 @@ plugins {
     id("com.android.application") version "7.2.0-rc02" apply false
     id("com.android.library") version "7.2.0-rc02" apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.21.0-RC2"
+}
+
+subprojects {
+    apply<DetektPlugin>()
+
+    detekt {
+        buildUponDefaultConfig = true
+        config = files("$rootDir/detekt.yml")
+    }
 }
 
 tasks.register("clean", Delete::class) {
